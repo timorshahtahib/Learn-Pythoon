66f39c3945054d1f8ffac0738d4520dcdf369818
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-02 18:00:35 +0330

    casting

--- varible.py
+++ varible.py
@@ -1,3 +1,14 @@
-x=1
-y=2
-print(1+2)
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+# casting
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+#end casting
