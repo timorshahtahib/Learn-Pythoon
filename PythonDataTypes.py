b7b02694f2e7f332448baf8bd2fe8cb4dc203b28
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-02 18:46:21 +0330

    type convert

--- PythonDataTypes.py
+++ PythonDataTypes.py
@@ -1,3 +1,38 @@
-x = 5
+x = 1
+y = 2.8
+z = 1j
 print(type(x))
+print(type(y))
+print(type(z))
 
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
