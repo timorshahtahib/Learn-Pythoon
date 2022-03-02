aafecb9acbbf62cb14cd883b72c773e485c60c79
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-02 18:50:54 +0330

    varible casting

---
+++ varibleCasting.py
@@ -0,0 +1,34 @@
+#integars
+print('integar')
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+
+
+#floats
+
+print('floats')
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+
+#strings
+print('strings')
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
