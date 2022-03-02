9d0dfaf0e03bddb82cea9ac02d0cc01ca6db275c
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-02 18:16:04 +0330

    Unpack a Collection

--- varible.py
+++ varible.py
@@ -34,3 +34,17 @@ print(a)
 print(b)
 print(c)
 print(d)
+
+
+
+
+#Unpack a Collection
+#If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+
+frutes=['apple','banan','orange']
+x,y,z=frutes
+print(x)
+print(y)
+print(z)
+
+
