f6077c02ad4f015a30a1df7c4a5eac5db5c72921
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-03 16:59:01 +0330

    String len

---
+++ strings.py
@@ -0,0 +1,23 @@
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+ magna aliqua."""
+print(a)
+
+
+
+b="timor"
+
+
+for x in b:
+    print(x)
+
+
+
+
+#String len
+
+c="timor"
+
+print(len(c))
