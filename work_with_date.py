8920894e7caeeae4561e01323c3ab2517d586ca8
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-09 16:40:15 +0330

    strftime

--- work_with_date.py
+++ work_with_date.py
@@ -1,8 +1,15 @@
 from datetime import date
-
+from datetime import datetime
 
 today=date.today()
 
 print("today time is {}".format(today))
 
 print("date is ",today.day,today.month,today.year)
+
+now=datetime.now()#comple today date
+print(now)
+
+print("year :" ,now.strftime("%Y"))
+
+print(now.strftime("%A,%d,%b,%Y"))#Wednesday,09,Mar,2022
