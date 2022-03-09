1ec682267074dd86e69d8600f832732c66f73cf4
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-09 16:45:20 +0330

    strftime

--- work_with_date.py
+++ work_with_date.py
@@ -13,3 +13,4 @@ print(now)
 print("year :" ,now.strftime("%Y"))
 
 print(now.strftime("%A,%d,%b,%Y"))#Wednesday,09,Mar,2022
+print(now.strftime("%A,%d,%b,%Y  - %I:%M:%S  %p"))#Wednesday,09,Mar,2022  - 04:45:04  PM
