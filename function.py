aad49456aaa8fde15eaae8c4868e7155942b5495
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-09 13:23:10 +0330

    fibbuntial pythoon

--- function.py
+++ function.py
@@ -1,4 +1,7 @@
 
+from unittest import result
+
+
 def sayhello():
     print("hi")   
 
@@ -6,5 +9,17 @@ def sayhello():
 sayhello()
 
 
+# 0,1,1,2,3,5,8,13 ......
+def fib(n):
+    
+    result=[]
+    a,b=0,1
+    while a<n:
+        result.append(a)
+        a,b=b,a+b
+        
+    print(result)
 
     
+    
+fib(100)
