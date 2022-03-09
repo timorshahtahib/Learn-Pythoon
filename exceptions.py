9c30720de299aae50164e6857d34d7c62be3f8a3
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-09 18:22:20 +0330

    file exception

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,6 @@
+import io
+
+
 def sum(a,b):
     assert(type(a) is int and type(b) is int),"numbers must be integer"
     assert(a>=0 and b>=0), "argumnets must be  positive"
@@ -18,10 +21,25 @@ def sum(a,b):
 
 
 
-try :
-    x=10
-    if x<12:
-        raise Exception("some ting wrong")
-except Exception as ex:
-    print("rese exception ", ex)
+# try :
+#     x=10
+#     if x<12:
+#         raise Exception("some ting wrong")
+# except Exception as ex:
+#     print("rese exception ", ex)
     
+    
+    
+try:
+      fn=open('text.txt','r')
+
+except FileNotFoundError as fn:
+      print(fn)
+except IOError as io_e:
+        print(io_e)
+else:
+        print("file opened")
+        fn.close()
+        
+        
+      
