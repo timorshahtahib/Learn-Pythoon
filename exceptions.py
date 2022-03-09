9ddacb96f94774dcd1d8799716e8970d98fda70f
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-09 18:11:03 +0330

    try_exception

--- exceptions.py
+++ exceptions.py
@@ -1,7 +1,17 @@
-def sun(a,b):
+def sum(a,b):
     assert(type(a) is int and type(b) is int),"numbers must be integer"
+    assert(a>=0 and b>=0), "argumnets must be  positive"
     return a+b
 
 
 
-print(sun(1,2))
+# print(sum(1,0))
+
+
+try:
+     print(sum(1,12))
+ 
+except AssertionError as error:
+    print("assertion error",error)
+except:
+  print('An exception occurred')
