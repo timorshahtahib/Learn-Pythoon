b346ca8399a37676a30f4f83fa51e0e662bd794e
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-09 17:46:25 +0330

    one year later

--- work_with_date.py
+++ work_with_date.py
@@ -1,6 +1,8 @@
 from datetime import date
 from datetime import datetime
 
+from datetime import timedelta
+
 today=date.today()
 
 print("today time is {}".format(today))
@@ -14,3 +16,7 @@ print("year :" ,now.strftime("%Y"))
 
 print(now.strftime("%A,%d,%b,%Y"))#Wednesday,09,Mar,2022
 print(now.strftime("%A,%d,%b,%Y  - %I:%M:%S  %p"))#Wednesday,09,Mar,2022  - 04:45:04  PM
+
+oneyear=datetime.now()+timedelta(days=365,hours=2)#2023-03-09 19:46:03.139444
+
+print("one year later" ,oneyear)
